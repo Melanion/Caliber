9360997d98de17ced53039564054d1f9077e209f
Melanion <megakeen@gmail.com>
2014-03-21 02:35:51 -0400

    Admin stuff and model unicode

--- vending/admin.py
+++ vending/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from vending.models import Round
 
 # Register your models here.
+admin.site.register(Round)
--- vending/models.py
+++ vending/models.py
@@ -6,18 +6,30 @@ class Company(models.Model):
     name = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
 
+    def __unicode__(self):
+        return self.name
+
 class Weapon(models.Model):
     name = models.CharField(max_length=50)
     weapon_type = models.CharField(max_length=50)
     manufacturer = models.ForeignKey(Company)
 
+    def __unicode__(self):
+        return self.name
+
 class Caliber(models.Model):
     name = models.CharField(max_length=50)
     weapon_type = models.ForeignKey(Weapon)
 
+    def __unicode__(self):
+        return self.name
+
 class Purpose(models.Model):
     description = models.CharField(max_length=50)
     round_name = models.ForeignKey('Round', related_name='round_name_set')
+
+    def __unicode__(self):
+        return self.description
 
 class Round(models.Model):
     name = models.CharField(max_length=50)
@@ -25,3 +37,6 @@ class Round(models.Model):
     purpose = models.ForeignKey(Purpose)
     year = models.DateField('date created')
     manufacturer = models.ForeignKey(Company)
+
+    def __unicode__(self):
+        return self.name
