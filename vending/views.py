0c5ae462ca7733844c27a8101e6d6ff47d14e14c
Melanion <megakeen@gmail.com>
2014-03-21 14:18:05 -0400

    Fixed an index error and modiefied views for details pages

--- vending/views.py
+++ vending/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.http import Http404
 
 from vending.models import Round, Caliber, Purpose, Weapon, Company
 
 # Create your views here.
 def index(request):
-    
+
     company_list = Company.objects.all()
     purpose_list = Purpose.objects.all()
     weapon_list = Weapon.objects.all()
     caliber_list = Caliber.objects.all()
     round_list = Round.objects.all()
-    context = {'round_list': round_list, 
+    context = {'round_list': round_list,
                'caliber_list':caliber_list,
                'purpose_list': purpose_list,
                'weapon_list':weapon_list,
                'company_list':company_list,}
     return render(request, 'vending/index.html', context)
+
+def detail(request, round_id):
+    round = get_object_or_404(Round, pk=round_id)
+    return render(request, 'vending/detail.html', {'round': round})
