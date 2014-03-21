53bcea24b7f509b1f9013d8432e98e9ba3543786
Melanion <megakeen@gmail.com>
2014-03-21 12:20:22 -0400

    Basic view for listing rounds

--- vending/views.py
+++ vending/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from vending.models import Round
 
 # Create your views here.
+def index(request):
+    round_list = Round.objects.all()
+    context = {'round_list': round_list}
+    return render(request, 'vending/index.html', context)
