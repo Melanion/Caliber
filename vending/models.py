38c68a9779f108b1b9594be069d039544ccee6ca
Melanion <megakeen@gmail.com>
2014-03-21 02:14:08 -0400

    Vending app and working Heroku database integration

---
+++ vending/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+class Weapon(models.Model):
+    name = models.CharField(max_length=50)
+    weapon_type = models.CharField(max_length=50)
+    manufacturer = models.ForeignKey(Company)
+
+class Caliber(models.Model):
+    name = models.CharField(max_length=50)
+    weapon_type = models.ForeignKey(Weapon)
+
+class Purpose(models.Model):
+    description = models.CharField(max_length=50)
+    round_name = models.ForeignKey('Round', related_name='round_name_set')
+
+class Round(models.Model):
+    name = models.CharField(max_length=50)
+    caliber = models.ForeignKey(Caliber)
+    purpose = models.ForeignKey(Purpose)
+    year = models.DateField('date created')
+    manufacturer = models.ForeignKey(Company)
