2f7410d194bf0347c7cc61448b0fa673c696c338
Wes Johnson <megakeen@gmail.com>
2014-03-21 10:23:38 -0400

    Added all models to admin

--- vending/admin.py
+++ vending/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from vending.models import Round
+from vending.models import Caliber
+from vending.models import Company
+from vending.models import Purpose
+from vending.models import Weapon
 
 # Register your models here.
 admin.site.register(Round)
+admin.site.register(Caliber)
+admin.site.register(Company)
+admin.site.register(Purpose)
+admin.site.register(Weapon)
