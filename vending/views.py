dfb681268be183be926dc86c58f753fc184e5657
Drew Jaqua <djaqua@smnet.net>
2014-03-21 13:53:17 -0400

    Implemented other dropdown menus

--- vending/views.py
+++ vending/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from vending.models import Round
+from vending.models import Round, Caliber, Purpose, Weapon, Company
 
 # Create your views here.
 def index(request):
+    
+    company_list = Company.objects.all()
+    purpose_list = Purpose.objects.all()
+    weapon_list = Weapon.objects.all()
+    caliber_list = Caliber.objects.all()
     round_list = Round.objects.all()
-    context = {'round_list': round_list}
+    context = {'round_list': round_list, 
+               'caliber_list':caliber_list,
+               'purpose_list': purpose_list,
+               'weapon_list':weapon_list,
+               'company_list':company_list,}
     return render(request, 'vending/index.html', context)
