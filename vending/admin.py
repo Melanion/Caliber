62aaa83cb71830cba2a6a49045a6a1cabc493b42
Drew Jaqua <djaqua@smnet.net>
2014-04-23 05:14:36 -0400

    Implemented basic Django models

--- vending/admin.py
+++ vending/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from vending.models import Round
 from vending.models import Caliber
-from vending.models import Company
-from vending.models import Purpose
-from vending.models import Weapon
+from vending.models import Manufacturer
+from vending.models import Material
+from vending.models import Bullet
+from vending.models import Cartridge
+
 
 # Register your models here.
 admin.site.register(Round)
 admin.site.register(Caliber)
-admin.site.register(Company)
-admin.site.register(Purpose)
-admin.site.register(Weapon)
+admin.site.register(Manufacturer)
+admin.site.register(Material)
+admin.site.register(Bullet)
+admin.site.register(Cartridge)
--- vending/models.py
+++ vending/models.py
@@ -2,38 +2,49 @@ from django.db import models
 
 # Create your models here.
 
-class Company(models.Model):
+
+class Manufacturer(models.Model):
+    ''' 
+    '''
     name = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
+    website = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.name
 
-class Weapon(models.Model):
-    name = models.CharField(max_length=50)
+class Material(models.Model):
+    name = models.CharField(max_length=16);    
 
     def __unicode__(self):
         return self.name
+
+class Bullet(models.Model):
+    purpose = models.CharField(max_length=32)
+
+    def __unicode__(self):
+        return self.purpose
 
 class Caliber(models.Model):
-    name = models.CharField(max_length=50)
-    weapon_type = models.ForeignKey(Weapon)
-
-    def __unicode__(self):
-        return self.name
-
-class Purpose(models.Model):
     description = models.CharField(max_length=50)
 
     def __unicode__(self):
         return self.description
 
+class Cartridge(models.Model):
+    rimfire = models.BooleanField(default=1)
+    
+    caliber = models.ForeignKey(Caliber)
+    material = models.ForeignKey(Material)
+    bullet = models.ForeignKey(Bullet)
+
 class Round(models.Model):
     name = models.CharField(max_length=50)
-    caliber = models.ForeignKey(Caliber)
-    purpose = models.ForeignKey(Purpose)
-    year = models.IntegerField(max_length=4)
-    manufacturer = models.ForeignKey(Company)
+    cost = models.DecimalField( max_digits=5, decimal_places=2);
+    inventory_count = models.IntegerField(max_length=4)
+
+    manufacturer = models.ForeignKey(Manufacturer)
+    cartridge = models.ForeignKey( Cartridge ); 
 
     def __unicode__(self):
         return self.name
