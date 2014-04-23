105a48c07049f3df5a3ecfcb6b09a381aabf0571
Melanion <megakeen@gmail.com>
2014-04-22 22:28:13 -0400

    More Heroku DB stuff

--- caliber/settings.py
+++ caliber/settings.py
@@ -56,16 +56,16 @@ WSGI_APPLICATION = 'caliber.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'caliber',
-        'USER': 'wes',
-        'PASSWORD': 'nerdc4mp',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE':'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'caliber',
+#         'USER': 'wes',
+#         'PASSWORD': 'nerdc4mp',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
